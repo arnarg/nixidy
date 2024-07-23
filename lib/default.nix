d46f8b4aafb776377b838fb8b6745ddc80d8a52e
Arnar Gauti Ingason <arnarg@fastmail.com>
2024-07-23 17:08:01 +0200

    Remove old functions from lib

--- lib/default.nix
+++ lib/default.nix
@@ -14,14 +14,4 @@ in
     kustomize = import ./kustomize.nix params;
     helm = import ./helm.nix params;
     kube = import ./kube.nix params;
-
-    mkApplication = {
-      name,
-      namespace,
-      resources ? {},
-      YAMLs ? [],
-    }: {
-      inherit name namespace;
-      resources = lib.mkMerge [resources (self.resources.fromManifestYAMLs YAMLs)];
-    };
   })
