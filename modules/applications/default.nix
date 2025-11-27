8996db667de30da65956a5e033220936119c3bfc
Wael Nasreddine <wael.nasreddine@gmail.com>
2025-11-26 18:11:42 -0800

    refactor: shared syncPolicy options
    This removes duplication between the applications options and the
    appOfApps options.

--- modules/applications/default.nix
+++ modules/applications/default.nix
@@ -81,6 +81,8 @@ let
       filtered = lib.filter (val: val != null) (lib.mapAttrsToList (_: val: val) opts);
     in
     filtered;
+
+  syncPolicy = import ./syncPolicy.nix { inherit lib nixidyDefaults; };
 in
 {
   imports = [
@@ -95,6 +97,8 @@ in
   ];
 
   options = with lib; {
+    inherit syncPolicy;
+
     name = mkOption {
       type = types.str;
       default = name;
@@ -138,180 +142,6 @@ in
         default = null;
         apply = applyCmpOption (val: if val then "IgnoreExtraneous" else null);
         description = "Sets IgnoreExtraneous compare option for the application. Only setting it as `true` has any effect.";
-      };
-    };
-    syncPolicy = {
-      autoSync = {
-        enable = mkOption {
-          type = types.bool;
-          default = nixidyDefaults.syncPolicy.autoSync.enable;
-          defaultText = literalExpression "config.nixidy.defaults.syncPolicy.autoSync.enable";
-          description = ''
-            Specifies if application should automatically sync.
-          '';
-        };
-        prune = mkOption {
-          type = types.bool;
-          default = nixidyDefaults.syncPolicy.autoSync.prune;
-          defaultText = literalExpression "config.nixidy.defaults.syncPolicy.autoSync.prune";
-          description = ''
-            Specifies if resources should be pruned during auto-syncing.
-          '';
-        };
-        selfHeal = mkOption {
-          type = types.bool;
-          default = nixidyDefaults.syncPolicy.autoSync.selfHeal;
-          defaultText = literalExpression "config.nixidy.defaults.syncPolicy.autoSync.selfHeal";
-          description = ''
-            Specifies if partial app sync should be executed when resources are changed only in
-            target Kubernetes cluster and no git change detected.
-          '';
-        };
-      };
-      managedNamespaceMetadata = mkOption {
-        type = types.nullOr (
-          types.submodule {
-            options = {
-              annotations = mkOption {
-                type = types.nullOr (types.attrsOf types.str);
-                description = ''
-                  Annotations to add to the ArgoCD managed namespace.
-                '';
-                default = null;
-              };
-              labels = mkOption {
-                type = types.nullOr (types.attrsOf types.str);
-                description = ''
-                  Label to add to the ArgoCD managed namespace.
-                '';
-                default = null;
-              };
-            };
-
-            config = { };
-          }
-        );
-        default = null;
-        description = ''
-          ArgoCD Managed namespace metadata.
-        '';
-      };
-      retry = mkOption {
-        type = types.nullOr (
-          types.submodule {
-            options = {
-              backoff = mkOption {
-                type = types.nullOr (
-                  types.submodule {
-                    options = {
-                      duration = mkOption {
-                        type = types.nullOr types.str;
-                        default = null;
-                      };
-                      factor = mkOption {
-                        type = types.nullOr types.int;
-                        default = null;
-                      };
-                      maxDuration = mkOption {
-                        type = types.nullOr types.str;
-                        default = null;
-                      };
-                    };
-
-                    config = { };
-                  }
-                );
-
-                default = null;
-              };
-
-              limit = mkOption {
-                type = (types.nullOr types.int);
-                default = null;
-              };
-            };
-
-            config = { };
-          }
-        );
-
-        default = null;
-        description = ''
-          ArgoCD retry syncPolicy.
-        '';
-      };
-      syncOptions = {
-        applyOutOfSyncOnly = mkOption {
-          type = types.bool;
-          default = false;
-          apply = val: if val then "ApplyOutOfSyncOnly=true" else null;
-          description = ''
-            Currently when syncing using auto sync Argo CD applies every object in the application.
-            For applications containing thousands of objects this takes quite a long time and puts undue pressure on the api server.
-            Turning on selective sync option which will sync only out-of-sync resources.
-          '';
-        };
-        createNamespace = mkOption {
-          type = types.bool;
-          default = false;
-          apply = val: if val then "CreateNamespace=true" else null;
-          description = ''
-            Namespace Auto-Creation ensures that namespace specified as the
-            application destination exists in the destination cluster.
-          '';
-        };
-        pruneLast = mkOption {
-          type = types.bool;
-          default = false;
-          apply = val: if val then "PruneLast=true" else null;
-          description = ''
-            This feature is to allow the ability for resource pruning to happen as a final, implicit wave of a sync operation,
-            after the other resources have been deployed and become healthy, and after all other waves completed successfully.
-          '';
-        };
-        replace = mkOption {
-          type = types.bool;
-          default = false;
-          apply = val: if val then "Replace=true" else null;
-          description = ''
-            By default, Argo CD executes `kubectl apply` operation to apply the configuration stored in Git.
-            In some cases `kubectl apply` is not suitable. For example, resource spec might be too big and won't fit into
-            `kubectl.kubernetes.io/last-applied-configuration` annotation that is added by kubectl apply.
-
-            If the `replace = true;` sync option is set the Argo CD will use `kubectl replace` or `kubectl create` command
-            to apply changes.
-          '';
-        };
-        serverSideApply = mkOption {
-          type = types.bool;
-          default = false;
-          apply = val: if val then "ServerSideApply=true" else null;
-          description = ''
-            By default, Argo CD executes `kubectl apply` operation to apply the configuration stored in Git.
-            This is a client side operation that relies on `kubectl.kubernetes.io/last-applied-configuration` annotation to
-            store the previous resource state.
-
-            If `serverSideApply = true;` sync option is set, Argo CD will use `kubectl apply --server-side` command to apply changes.
-
-            More info [here](https://argo-cd.readthedocs.io/en/stable/user-guide/sync-options/#server-side-apply).
-          '';
-        };
-        failOnSharedResource = mkOption {
-          type = types.bool;
-          default = false;
-          apply = val: if val then "FailOnSharedResource=true" else null;
-          description = ''
-            By default, Argo CD will apply all manifests found in the git path configured in the Application regardless if the
-            resources defined in the yamls are already applied by another Application. If the `failOnSharedResource` sync option
-            is set, Argo CD will fail the sync whenever it finds a resource in the current Application that is already applied in
-            the cluster by another Application.
-          '';
-        };
-      };
-      finalSyncOpts = mkOption {
-        type = types.listOf types.str;
-        default = [ ];
-        internal = true;
       };
     };
     destination = {
--- modules/nixidy.nix
+++ modules/nixidy.nix
@@ -15,6 +15,8 @@ let
       filtered = lib.filter (val: val != null) (lib.mapAttrsToList (_: val: val) opts);
     in
     filtered;
+
+  syncPolicy = import ./applications/syncPolicy.nix { inherit lib nixidyDefaults; };
 in
 {
   imports = [
@@ -160,6 +162,7 @@ in
     };
 
     appOfApps = {
+      inherit syncPolicy;
       name = mkOption {
         type = types.str;
         default = "apps";
@@ -193,184 +196,6 @@ in
           '';
         };
       };
-      syncPolicy = {
-        autoSync = {
-          enable = mkOption {
-            type = types.bool;
-            # TODO: Honor the default?
-            # default = nixidyDefaults.syncPolicy.autoSync.enable;
-            # defaultText = literalExpression "config.nixidy.defaults.syncPolicy.autoSync.enable";
-            default = true; # XXX: Previous behavior, worth keeping?
-            description = ''
-              Specifies if application should automatically sync.
-            '';
-          };
-          prune = mkOption {
-            type = types.bool;
-            default = nixidyDefaults.syncPolicy.autoSync.prune;
-            defaultText = literalExpression "config.nixidy.defaults.syncPolicy.autoSync.prune";
-            description = ''
-              Specifies if resources should be pruned during auto-syncing.
-            '';
-          };
-          selfHeal = mkOption {
-            type = types.bool;
-            # TODO: Honor the default?
-            # default = nixidyDefaults.syncPolicy.autoSync.selfHeal;
-            # defaultText = literalExpression "config.nixidy.defaults.syncPolicy.autoSync.selfHeal";
-            default = true; # XXX: Previous behavior, worth keeping?
-            description = ''
-              Specifies if partial app sync should be executed when resources are changed only in
-              target Kubernetes cluster and no git change detected.
-            '';
-          };
-        };
-        managedNamespaceMetadata = mkOption {
-          type = types.nullOr (
-            types.submodule {
-              options = {
-                annotations = mkOption {
-                  type = types.nullOr (types.attrsOf types.str);
-                  description = ''
-                    Annotations to add to the ArgoCD managed namespace.
-                  '';
-                  default = null;
-                };
-                labels = mkOption {
-                  type = types.nullOr (types.attrsOf types.str);
-                  description = ''
-                    Label to add to the ArgoCD managed namespace.
-                  '';
-                  default = null;
-                };
-              };
-
-              config = { };
-            }
-          );
-          default = null;
-          description = ''
-            ArgoCD Managed namespace metadata.
-          '';
-        };
-        retry = mkOption {
-          type = types.nullOr (
-            types.submodule {
-              options = {
-                backoff = mkOption {
-                  type = types.nullOr (
-                    types.submodule {
-                      options = {
-                        duration = mkOption {
-                          type = types.nullOr types.str;
-                          default = null;
-                        };
-                        factor = mkOption {
-                          type = types.nullOr types.int;
-                          default = null;
-                        };
-                        maxDuration = mkOption {
-                          type = types.nullOr types.str;
-                          default = null;
-                        };
-                      };
-
-                      config = { };
-                    }
-                  );
-
-                  default = null;
-                };
-
-                limit = mkOption {
-                  type = (types.nullOr types.int);
-                  default = null;
-                };
-              };
-
-              config = { };
-            }
-          );
-
-          default = null;
-          description = ''
-            ArgoCD retry syncPolicy.
-          '';
-        };
-        syncOptions = {
-          applyOutOfSyncOnly = mkOption {
-            type = types.bool;
-            default = false;
-            apply = val: if val then "ApplyOutOfSyncOnly=true" else null;
-            description = ''
-              Currently when syncing using auto sync Argo CD applies every object in the application.
-              For applications containing thousands of objects this takes quite a long time and puts undue pressure on the api server.
-              Turning on selective sync option which will sync only out-of-sync resources.
-            '';
-          };
-          createNamespace = mkOption {
-            type = types.bool;
-            default = false;
-            apply = val: if val then "CreateNamespace=true" else null;
-            description = ''
-              Namespace Auto-Creation ensures that namespace specified as the
-              application destination exists in the destination cluster.
-            '';
-          };
-          pruneLast = mkOption {
-            type = types.bool;
-            default = false;
-            apply = val: if val then "PruneLast=true" else null;
-            description = ''
-              This feature is to allow the ability for resource pruning to happen as a final, implicit wave of a sync operation,
-              after the other resources have been deployed and become healthy, and after all other waves completed successfully.
-            '';
-          };
-          replace = mkOption {
-            type = types.bool;
-            default = false;
-            apply = val: if val then "Replace=true" else null;
-            description = ''
-              By default, Argo CD executes `kubectl apply` operation to apply the configuration stored in Git.
-              In some cases `kubectl apply` is not suitable. For example, resource spec might be too big and won't fit into
-              `kubectl.kubernetes.io/last-applied-configuration` annotation that is added by kubectl apply.
-
-              If the `replace = true;` sync option is set the Argo CD will use `kubectl replace` or `kubectl create` command
-              to apply changes.
-            '';
-          };
-          serverSideApply = mkOption {
-            type = types.bool;
-            default = false;
-            apply = val: if val then "ServerSideApply=true" else null;
-            description = ''
-              By default, Argo CD executes `kubectl apply` operation to apply the configuration stored in Git.
-              This is a client side operation that relies on `kubectl.kubernetes.io/last-applied-configuration` annotation to
-              store the previous resource state.
-
-              If `serverSideApply = true;` sync option is set, Argo CD will use `kubectl apply --server-side` command to apply changes.
-
-              More info [here](https://argo-cd.readthedocs.io/en/stable/user-guide/sync-options/#server-side-apply).
-            '';
-          };
-          failOnSharedResource = mkOption {
-            type = types.bool;
-            default = false;
-            apply = val: if val then "FailOnSharedResource=true" else null;
-            description = ''
-              By default, Argo CD will apply all manifests found in the git path configured in the Application regardless if the
-              resources defined in the yamls are already applied by another Application. If the `failOnSharedResource` sync option
-              is set, Argo CD will fail the sync whenever it finds a resource in the current Application that is already applied in
-              the cluster by another Application.
-            '';
-          };
-        };
-        finalSyncOpts = mkOption {
-          type = types.listOf types.str;
-          default = [ ];
-          internal = true;
-        };
-      };
     };
 
     charts = mkOption {
@@ -395,7 +220,15 @@ in
   };
 
   config = {
-    nixidy.appOfApps.syncPolicy.finalSyncOpts = convertSyncOptionsList cfg.appOfApps.syncPolicy.syncOptions;
+    nixidy.appOfApps = {
+      # XXX: Restore the previous behavior where autoSync/selfHeal is enabled.
+      syncPolicy.autoSync = {
+        enable = lib.mkDefault true;
+        selfHeal = lib.mkDefault true;
+      };
+
+      syncPolicy.finalSyncOpts = convertSyncOptionsList cfg.appOfApps.syncPolicy.syncOptions;
+    };
 
     applications.${cfg.appOfApps.name} = {
       inherit (cfg.appOfApps) namespace;
