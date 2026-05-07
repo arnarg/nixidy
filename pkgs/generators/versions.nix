5299b3e41f87e24b5d684ea88e5cfd03fe403071
Arnar Gauti Ingason <arnarg@fastmail.com>
2026-05-07 16:54:19 +0200

    chore: update kubernetes versions

--- pkgs/generators/versions.nix
+++ pkgs/generators/versions.nix
@@ -1,12 +1,4 @@
 {
-  "1.31.14" = {
-    hash = "sha256-rfYv9VlXKdQNcR1AiMv7wVeyJYHq5qJPw9g15sn3U40=";
-    spec = "api/openapi-spec/swagger.json";
-    discovery = {
-      core = "api/discovery/api__v1.json";
-      aggregated = "api/discovery/aggregated_v2.json";
-    };
-  };
   "1.32.11" = {
     hash = "sha256-/Hg0yf7Y8My+g3w36fA94ozCzhmI0sMjpF7FPZ/UykY=";
     spec = "api/openapi-spec/swagger.json";
@@ -15,24 +7,32 @@
       aggregated = "api/discovery/aggregated_v2.json";
     };
   };
-  "1.33.7" = {
-    hash = "sha256-439MCwbt6XRhKi+6wbyro33ptS//uIv0TeL2NQ/tD7M=";
+  "1.33.11" = {
+    hash = "sha256-wkyIUxs1wtJLLBJq1PQtRErTX3AxhmOxIx4gM9w/K00=";
     spec = "api/openapi-spec/swagger.json";
     discovery = {
       core = "api/discovery/api__v1.json";
       aggregated = "api/discovery/aggregated_v2.json";
     };
   };
-  "1.34.1" = {
-    hash = "sha256-1X2kxUi7jNnyTNo+ZGZmhU5DHVl98eYOrJ4qpjzqqjE=";
+  "1.34.7" = {
+    hash = "sha256-pDSVKgFIcMUDiy0BWx/VadwuatteUwmctaNbCLY4Yhg=";
     spec = "api/openapi-spec/swagger.json";
     discovery = {
       core = "api/discovery/api__v1.json";
       aggregated = "api/discovery/aggregated_v2.json";
     };
   };
-  "1.35.0" = {
-    hash = "sha256-AT1/4RhnVK/mAoNVqPIfSwbzD8VNRqKumOpE0fidJ74=";
+  "1.35.4" = {
+    hash = "sha256-UXYkReGD77Uu0P0iYvkK58Uj0f7CuXGMb1WJBD7/61U=";
+    spec = "api/openapi-spec/swagger.json";
+    discovery = {
+      core = "api/discovery/api__v1.json";
+      aggregated = "api/discovery/aggregated_v2.json";
+    };
+  };
+  "1.36.0" = {
+    hash = "sha256-6waSybeFc6xMIT93WLR1OPN/bhcHzvUzJGZliEuEQIM=";
     spec = "api/openapi-spec/swagger.json";
     discovery = {
       core = "api/discovery/api__v1.json";
