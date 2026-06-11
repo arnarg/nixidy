4fa1f532b8e662334521afb89ec1d3ab815a9fd1
Jason Bowman <jason@json64.dev>
2026-06-11 13:11:35 -0700

    refactor(generators): extract k8s acquisition into sources/, slim default.nix to wiring

--- pkgs/generators/default.nix
+++ pkgs/generators/default.nix
@@ -8,172 +8,21 @@ let
 
   crdSrc = import ./sources/crd.nix { inherit pkgs lib klib; };
   chartSrc = import ./sources/chart.nix { inherit pkgs lib klib; };
+  k8sSrc = import ./sources/k8s.nix { inherit pkgs lib; };
 
   #########
   ## K8s ##
   #########
-  fromSchema =
-    name: schema:
-    import ./compile/generator.nix {
-      inherit
-        pkgs
-        lib
-        name
-        schema
-        ;
-
-      # The ports list in Container, EphemeralContainer
-      # and ServiceSpec should not enforce "protocol".
-      # See: https://github.com/arnarg/nixidy/issues/34
-      specialMapKeys = {
-        "io.k8s.api.core.v1.Container".ports = [ "containerPort" ];
-        "io.k8s.api.core.v1.EphemeralContainer".ports = [ "containerPort" ];
-        "io.k8s.api.core.v1.ServiceSpec".ports = [ "port" ];
-      };
-
-      definitionsOverlay = final: prev: {
-        "io.k8s.apimachinery.pkg.api.resource.Quantity" = {
-          inherit (prev."io.k8s.apimachinery.pkg.api.resource.Quantity") description;
-          oneOf = [
-            { type = "string"; }
-            { type = "number"; }
-          ];
-        };
-      };
-    };
-
-  genNamespaced =
-    core: aggregated:
-    let
-      core' =
-        let
-          data = builtins.fromJSON (builtins.readFile core);
-        in
-        {
-          core = {
-            ${data.groupVersion} = lib.mergeAttrsList (
-              lib.concatMap (
-                res:
-                lib.optional (res.singularName != "") {
-                  ${res.kind} = res.namespaced;
-                }
-              ) data.resources
-            );
-          };
-        };
-
-      aggregated' =
-        let
-          data = builtins.fromJSON (builtins.readFile aggregated);
-        in
-        lib.mergeAttrsList (
-          map (item: {
-            ${item.metadata.name} = lib.mergeAttrsList (
-              map (version: {
-                ${version.version} = lib.mergeAttrsList (
-                  map (res: {
-                    ${res.responseKind.kind} = res.scope == "Namespaced";
-                  }) version.resources
-                );
-              }) item.versions
-            );
-          }) data.items
-        );
-    in
-    core' // aggregated';
-
-  genRoots =
-    with lib;
-    swagger: namespaced:
-    let
-      refType = attr: head (tail (tail (splitString "/" attr."$ref")));
-
-      refDefinition = attr: head (tail (tail (splitString "/" attr."$ref")));
-
-      mapCharPairs =
-        f: s1: s2:
-        concatStrings (
-          imap0 (i: c1: f i c1 (if i >= stringLength s2 then "" else elemAt (stringToCharacters s2) i)) (
-            stringToCharacters s1
-          )
-        );
-
-      getAttrName =
-        resource: kind:
-        mapCharPairs (
-          i: c1: c2:
-          if lib.hasPrefix "API" kind && i == 0 then
-            "A"
-          else if i == 0 then
-            c1
-          else if c2 == "" || (lib.toLower c2) != c1 then
-            c1
-          else
-            c2
-        ) resource kind;
-    in
-    mapAttrs'
-      (
-        name: path:
-        let
-          ref = refType (head path.post.parameters).schema;
-          name' = last (splitString "/" name);
-          group' = path.post."x-kubernetes-group-version-kind".group;
-          group = if group' == "" then "core" else group';
-          version = path.post."x-kubernetes-group-version-kind".version;
-          kind = path.post."x-kubernetes-group-version-kind".kind;
-          attrName = getAttrName name' kind;
-        in
-        nameValuePair ref {
-          inherit
-            ref
-            attrName
-            group
-            version
-            kind
-            ;
-          inherit (swagger.definitions.${ref}) description;
-
-          name = name';
-          definition = refDefinition (head path.post.parameters).schema;
-          namespaced = attrByPath [ group version kind ] false namespaced;
-        }
-      )
-      (
-        filterAttrs (
-          _name: path: hasAttr "post" path && path.post."x-kubernetes-action" == "post"
-        ) swagger.paths
-      );
-
   k8s = pkgs.linkFarm "k8s-generated" (
-    builtins.attrValues (
-      builtins.mapAttrs (
-        version: conf:
-        let
-          short = builtins.concatStringsSep "." (lib.lists.sublist 0 2 (builtins.splitVersion version));
-
-          src = pkgs.fetchFromGitHub {
-            owner = "kubernetes";
-            repo = "kubernetes";
-            rev = "v${version}";
-            hash = conf.hash;
-          };
-
-          namespaced = genNamespaced "${src}/${conf.discovery.core}" "${src}/${conf.discovery.aggregated}";
-
-          swagger = builtins.fromJSON (builtins.readFile "${src}/${conf.spec}");
-
-          schema = {
-            inherit (swagger) definitions;
-            roots = genRoots swagger namespaced;
-          };
-        in
-        {
-          name = "v${short}.nix";
-          path = fromSchema "v${short}" schema;
-        }
-      ) (import ./versions.nix)
-    )
+    map (v: {
+      name = "v${v.short}.nix";
+      path = import ./compile/generator.nix {
+        inherit pkgs lib;
+        name = "v${v.short}";
+        schema = v.schema;
+        inherit (k8sSrc.k8sCompileOptions) specialMapKeys definitionsOverlay;
+      };
+    }) k8sSrc.perVersion
   );
 
   #########
